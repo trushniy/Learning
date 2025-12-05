cd364a6300b5bf1d48d63c0a303aaaca6a2c905b
trushniy <mironovmakar246@gmail.com>
2025-12-05 22:32:50 +0300

    новые изучения новые питоны
    короче уроки посмоттрел записал что увидел как говориться

--- learning/tolstiy.py
+++ learning/tolstiy.py
@@ -1,1 +1,20 @@
-print('helloworld')
+# name = 250
+# print(name)
+# print(type(name))
+
+# age = int(input('введите число:'))
+# print('вот что ты ввел',age,type(age)) 
+
+
+# result = 3 + 5
+# print(result)
+
+
+# age = 10 
+# age = float(age)
+# print(age,type(age))
+
+
+num4 = int(input('введи что хочешь:'))
+num5 = int(input('тоже что хочешь:'))
+print(num4 + num5,'сложил хуйню',type(num4 + num5))
